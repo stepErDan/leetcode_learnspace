a81a21a65ac9deed3578d0fb8c8d002f49e4157e
dingx <dx402967115@qq.com>
2020-06-23 17:10:40 +0800

    问题24、67

---
+++ src/main/java/com/leetcode/learn/solution/easy/solution_067.java
@@ -0,0 +1,78 @@
+package com.leetcode.learn.solution.easy;
+
+/**
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ *
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ * 示例 2:
+ *
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-binary
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class solution_067 {
+    public void main(String[] args) {
+        addBinary("11","1");
+    }
+
+    /**
+     * @Author : own
+     * @param a
+     * @param b
+     * @return
+     */
+    public String addBinary(String a, String b) {
+        StringBuffer num = new StringBuffer();
+        char[] as = a.toCharArray();
+        char[] bs = b.toCharArray();
+        boolean addflag = false;
+        int maxLength = (as.length > bs.length)?as.length:bs.length;
+        int minLength = (as.length <= bs.length)?as.length:bs.length;
+        for(int i = maxLength;i >= 1;i--){
+            int index = maxLength - i;
+            int type = 0;
+            if(addflag){
+                type++;
+            }
+            if((as.length - index) > 0 && '1' == as[as.length - index - 1])
+                type++;
+            if((bs.length - index) > 0 && '1' == bs[bs.length - index - 1])
+                type++;
+            switch(type){
+                case 0:
+                    addflag = false;
+                    num.append("0");
+                    break;
+                case 1:
+                    addflag = false;
+                    num.append("1");
+                    break;
+                case 2:
+                    addflag = true;
+                    num.append("0");
+                    break;
+                case 3:
+                    addflag = true;
+                    num.append("1");
+                    break;
+            }
+        }
+
+        if(addflag){
+            num.append("1");
+        }
+
+        return num.reverse().toString();
+    }
+}
---
+++ src/main/java/com/leetcode/learn/solution/medium/solution_024.java
@@ -0,0 +1,97 @@
+package com.leetcode.learn.solution.medium;
+
+import com.leetcode.learn.model.ListNode;
+
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ *  
+ *
+ * 示例:
+ *
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class solution_024 {
+    public void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        swapPairs(head);
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+    public ListNode swapPairs(ListNode head) {
+        //交换双方的地址
+        return swap(head);
+    }
+
+    /**
+     * @Author : own
+     * @param head
+     * @return
+     */
+    public ListNode swap(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        //获取1的对象
+        ListNode FirstNode = head;
+        //获取2的对象
+        ListNode SecondNode = head.next;
+        //1的下一级指向3
+        FirstNode.next = swap(SecondNode.next);
+        //2的下一级指向1
+        SecondNode.next = FirstNode;
+        //递归调用
+        return SecondNode;
+    }
+
+    /**
+     * 解法2：迭代
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs_2(ListNode head) {
+
+        // Dummy node acts as the prevNode for the head node
+        // of the list and hence stores pointer to the head node.
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prevNode = dummy;
+
+        while ((head != null) && (head.next != null)) {
+
+            // Nodes to be swapped
+            ListNode firstNode = head;
+            ListNode secondNode = head.next;
+
+            // Swapping
+            prevNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+
+            // Reinitializing the head and prevNode for next swap
+            prevNode = firstNode;
+            head = firstNode.next; // jump
+        }
+
+        // Return the new head node.
+        return dummy.next;
+    }
+}
