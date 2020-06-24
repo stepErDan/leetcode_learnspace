d20fff08b76385ea7a626da1dc38c11b2472b31a
dingx <dx402967115@qq.com>
2020-06-24 23:26:10 +0800

    001/344/016 - 100%

--- src/main/java/com/leetcode/learn/solution/medium/solution_016.java
+++ src/main/java/com/leetcode/learn/solution/medium/solution_016.java
@@ -28,13 +28,93 @@ public class solution_016 {
         threeSumClosest(new int[]{1,2,4,8,16,32,64,128},82);
     }
 
+
+
     /**
-     *
+     * 最菜的暴力法完成打卡，今日耗时：4小时。。。。
      * @param nums
      * @param target
      * @return
      */
     public static int threeSumClosest(int[] nums, int target) {
+        //定义存放绝对值的数组
+        int abs = Math.abs(nums[0] + nums[1] + nums[2] - target),num = nums[0] + nums[1] + nums[2],newAbs;
+        //遍历处理
+        for(int i = 0;i < nums.length - 2;i++){ //最外层循环
+            for(int j = i + 1;j < nums.length - 1;j++){
+                for(int k = j + 1;k < nums.length;k++){
+                    newAbs = Math.abs(nums[i] + nums[j] + nums[k] - target);
+                    if(newAbs < abs){
+                        abs = newAbs;
+                        num = nums[i] + nums[j] + nums[k];
+                    }
+                }
+            }
+        }
+        return num;
+    }
+
+    /**
+     * 官方解法
+     * 1、先对nums进行排序
+     * 2、枚举第一个数，第二个数从第一个数后开始，第三个数从最后一个开始，
+     *     求和，看和大了，第三个数退一，和小了第二个数进一，直到全部遍历
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int threeSumClosest_2(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int best = 10000000;
+
+        // 枚举 a
+        for (int i = 0; i < n; ++i) {
+            // 保证和上一次枚举的元素不相等
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            // 使用双指针枚举 b 和 c
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                // 如果和为 target 直接返回答案
+                if (sum == target) {
+                    return target;
+                }
+                // 根据差值的绝对值来更新答案
+                if (Math.abs(sum - target) < Math.abs(best - target)) {
+                    best = sum;
+                }
+                if (sum > target) {
+                    // 如果和大于 target，移动 c 对应的指针
+                    int k0 = k - 1;
+                    // 移动到下一个不相等的元素
+                    while (j < k0 && nums[k0] == nums[k]) {
+                        --k0;
+                    }
+                    k = k0;
+                } else {
+                    // 如果和小于 target，移动 b 对应的指针
+                    int j0 = j + 1;
+                    // 移动到下一个不相等的元素
+                    while (j0 < k && nums[j0] == nums[j]) {
+                        ++j0;
+                    }
+                    j = j0;
+                }
+            }
+        }
+        return best;
+    }
+
+    /**
+     *错误解题思路，实际上适合两数求和，因为0和1绑定了。。。。。。。。。当然两数用不了这么复杂。。。。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int threeSumClosest_e_2(int[] nums, int target) {
         //定义存放绝对值的数组
         int abs = Math.abs(nums[0] + nums[1] + nums[2] - target),num = 0,newAbs;
         //定义结果集
