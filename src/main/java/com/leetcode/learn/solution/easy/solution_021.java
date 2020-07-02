8427b2dab4e424f58c834b41b75344282260b665
dingx <dx402967115@qq.com>
2020-07-02 15:43:00 +0800

    021 - 100%

---
+++ src/main/java/com/leetcode/learn/solution/easy/solution_021.java
@@ -0,0 +1,45 @@
+package com.leetcode.learn.solution.easy;
+
+import com.leetcode.learn.model.ListNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class solution_021 {
+    public static void main(String[] args) {
+        mergeTwoLists(new ListNode(new int[]{1,2,4}), new ListNode(new int[]{1,3,4}));
+    }
+
+    /**
+     * 递归法
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }else if(l2 == null){
+            return l1;
+        }
+        if(l1.val > l2.val){
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }else{
+            l1.next = mergeTwoLists(l2,l1.next);
+            return l1;
+        }
+    }
+}
