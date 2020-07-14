ffa0ecbd2f48a1696537f0ae911c897c9d3a462c
dingx <dx402967115@qq.com>
2020-07-14 23:03:19 +0800

    153 - 100%

---
+++ src/main/java/com/leetcode/learn/solution/medium/solution_153.java
@@ -0,0 +1,89 @@
+package com.leetcode.learn.solution.medium;
+
+/**
+ * 153. 寻找旋转排序数组中的最小值
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ *
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ *
+ * 请找出其中最小的元素。
+ *
+ * 你可以假设数组中不存在重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,4,5,1,2]
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入: [4,5,6,7,0,1,2]
+ * 输出: 0
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class solution_153 {
+    public static void main(String[] args) {
+        new solution_153().findMin(new int[]{1,2,3,0});
+    }
+
+    public int findMin(int[] nums) {
+        int r = nums.length - 1;
+        while(nums[r] > nums[r - 1]){
+            r--;
+        }
+        return nums[r];
+    }
+
+    /**
+     * 官方解题：二分查找
+     * @param nums
+     * @return
+     */
+    public int findMin_1(int[] nums) {
+        // If the list has just one element then return that element.
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        // initializing left and right pointers.
+        int left = 0, right = nums.length - 1;
+
+        // if the last element is greater than the first element then there is no rotation.
+        // e.g. 1 < 2 < 3 < 4 < 5 < 7. Already sorted array.
+        // Hence the smallest element is first element. A[0]
+        if (nums[right] > nums[0]) {
+            return nums[0];
+        }
+
+        // Binary search way
+        while (right >= left) {
+            // Find the mid element
+            int mid = left + (right - left) / 2;
+
+            // if the mid element is greater than its next element then mid+1 element is the smallest
+            // This point would be the point of change. From higher to lower value.
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+
+            // if the mid element is lesser than its previous element then mid element is the smallest
+            if (nums[mid - 1] > nums[mid]) {
+                return nums[mid];
+            }
+
+            // if the mid elements value is greater than the 0th element this means
+            // the least value is still somewhere to the right as we are still dealing with elements
+            // greater than nums[0]
+            if (nums[mid] > nums[0]) {
+                left = mid + 1;
+            } else {
+                // if nums[0] is greater than the mid value then this means the smallest value is somewhere to
+                // the left
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
