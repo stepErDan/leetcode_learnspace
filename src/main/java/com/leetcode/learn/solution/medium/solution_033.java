6f52164a7157331a020270353dca7d0d8a2629b6
dingx <dx402967115@qq.com>
2020-07-17 11:14:11 +0800

    033 - 100%

--- src/main/java/com/leetcode/learn/solution/medium/solution_033.java
+++ src/main/java/com/leetcode/learn/solution/medium/solution_033.java
@@ -35,18 +35,25 @@ public class solution_033 {
         int mid,l = 0,r = nums.length - 1;
         while(l <= r){
             mid = l + ((r - l) >> 1);
-            if(target == nums[mid]){
+            if(nums[mid] == target){
                 return mid;
-            }else if(target >= nums[0] && target < nums[mid]){
-                r = mid - 1;
-            }else if(target >= nums[0] && target > nums[mid]){
-                if(nums[mid] > nums[0]){
+            }
+            if(nums[mid] >= nums[0]){
+                if(target > nums[mid]){
+                    l = mid + 1;
+                }else if(target < nums[mid] && target >= nums[0]){
+                    r = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+            }else{
+                if(target < nums[mid]){
+                    r = mid - 1;
+                }else if(target > nums[mid] && target < nums[0]){
                     l = mid + 1;
                 }else{
                     r = mid - 1;
                 }
-            }else if(nums[mid] > target && target <= nums[nums.length - 1]){
-                l = mid + 1;
             }
         }
         return -1;
